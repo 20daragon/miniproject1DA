77ee384d68cd3612f844c398a577eb60fc06bf6a
Derrick Aragon <daaragon2@mail.fhsu.edu>
2023-09-17 21:58:31 -0500

    Added charts file so now the charts automatically save

--- main.py
+++ main.py
@@ -40,6 +40,8 @@ for stock in stocks:
 
     days = list(range(1, len(stockClosing)+1))
 
+    # (10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
+
     # Plots the graph
     plt.plot(days, stockClosing)
 
@@ -57,12 +59,18 @@ for stock in stocks:
     plt.xlabel(days)
     plt.ylabel("Closing Price")
     plt.title("Closing Price for " + stock)
+
+    # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
+
+    # Saves the charts before showing so it doesn't appear blank
+    savefile = "charts/" + stock + ".png"
+    plt.savefig(savefile)
+
+    # Shows the actual graphs
     plt.show()
 
 
 
-#(10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the documentation for matplotlib. At minimum it just needs to show 10 data points.
-#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
 #(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 #(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 #(20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
